27c061fa58db176a4a971f924efe288edb4445a9
raulathallah <raultaqi@gmail.com>
2024-10-21 14:33:07 +0700

    learning jsx

--- react-vite-application/src/App.jsx
+++ react-vite-application/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Belajar from './components/Belajar'
+import { haloJakarta as jkt, haloBandung } from './functions/halo'
+import * as sapa from './functions/halo';
 
 function App() {
 
@@ -94,9 +96,9 @@ function App() {
   console.log(arrayKuadrat);
 
   const mahasiswaX = [
-    {nama: "Eka", umur: 19, jurusan: "IT"},
-    {nama: "Lisa", umur: 20, jurusan: "SI"},
-    {nama: "Rudi", umur: 21, jurusan: "IBM"}
+    {nama: "Eka", umur: 19, jurusan: "Teknik Informatika"},
+    {nama: "Lisa", umur: 20, jurusan: "Sistem Informasi"},
+    {nama: "Rudi", umur: 21, jurusan: "Business management"}
   ];
 
   const prosesMahasiswaX = (mahasiswa) => `${mahasiswa.nama} (${mahasiswa.umur}), ${mahasiswa.jurusan} -- `
@@ -104,6 +106,73 @@ function App() {
 
   console.log(formatMahasiswaX);
 
+
+  const getUser = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+        resolve("Rudi");
+        reject("Something wrong...")
+      }, 2000)
+    })
+  }
+  
+
+  // console.log("Start...");
+  // getUser().then((userName)=>console.log(userName)).catch((err)=>console.log(err));
+  // console.log("Finish");
+
+  const tryGetName = async ()=> {
+    let userName = await getUser();
+    console.log(userName);
+  }
+
+  console.log("(2)Start...")
+  tryGetName();
+  console.log("Finish")
+
+  console.log(jkt());
+  console.log(haloBandung());
+  console.log(sapa.haloJakarta());
+
+  const myElement = <ul style={{textAlign: 'left'}}>{mahasiswaX.map((mhs, i)=> <li key={i}><a href="https://google.com">{`${mhs.nama} (${mhs.umur}), ${mhs.jurusan}`}</a></li>)}</ul>
+  const myImage = <img src='public/bg.jpg'/>;
+  const fragment = (
+    <>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+    </>
+  )
+  let myText ="Belajar React!"
+  const myElemetX = 
+    (
+    <div>
+      <h1>{myText}</h1>
+      <p>10 + 5 = {10 + 5}</p>
+      <span title={myText} style={{color: 'aqua'}}>React Uncover</span>
+    </div>
+    )
+
+  let nilai = 30;
+  let pesan = nilai > 70 ? <p>Congratulations!</p> : <p>Try Again!</p>
+
+
+  let mahasiswaTable = (
+    <table>
+      <tr>
+        <th>Nama</th>
+        <th>Umur</th>
+        <th>Jurusan</th>
+      </tr>
+        {mahasiswaX.map((value) =>
+          <tr id="data" onClick={() => alert(`Nama: ${value.nama} (${value.umur}), Jurusan: ${value.jurusan}`)}>
+            <td>{value.nama}</td>
+            <td>{value.umur}</td>
+            <td>{value.jurusan}</td>
+          </tr>
+        )}
+    </table>
+  )
   // ------------ || ------------
   const kuadrat = (a) => {
     return a*a;
@@ -114,7 +183,15 @@ function App() {
   return (
     <>
       {/* <Belajar /> */}
-      {formatMahasiswaX}
+      {/* {formatMahasiswaX} */}
+      {/* {myElement} */}
+      {/* {myImage}
+      {fragment}
+      {myElemetX}
+      {nilai}
+      {pesan} */}
+
+      {mahasiswaTable}
     </>
   )
 }
---
+++ react-vite-application/src/functions/halo.js
@@ -0,0 +1,8 @@
+const haloJakarta = ()=> {
+    return "Halo Jakarta";
+}
+const haloBandung = ()=> {
+    return "Halo Bandung";
+}
+export {haloJakarta, haloBandung};
+export default haloJakarta;
